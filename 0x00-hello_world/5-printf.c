c02bfe517b30154b0e6cf97ee02c6dca23cded14
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-10-05 14:46:24 -0700

    cm

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-/* main - Prints out a string to stdout. Return: 0 if success. */
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
+
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
-
+	return (0);
+}
