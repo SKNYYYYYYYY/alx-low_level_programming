370ca302f8f6c279a0a1635d11ea99e66bc25d8d
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-10-05 08:09:30 -0700

    “script”

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,18 @@
-#include<stdio.h>
-int main (void)
+#include <stdio.h>
+
+/**
+ *  * main - Prints "with proper grammar, but the outcome is
+ *   *                a piece of art," followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
+int main(void)
 {
-	printf("with proper grammar, but the outome is a piece of art");
-		return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
 }
+
+
+
+
