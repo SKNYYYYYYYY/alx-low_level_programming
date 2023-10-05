95ea98a86e9da33cc525141233650c3a269da8f5
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-10-05 08:03:25 -0700

    “script”

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main (void)
 {
-	printf("with proper grammar, but the outome is a piece of art,")
+	printf("with proper grammar, but the outome is a piece of art");
 		return (0);
 }
