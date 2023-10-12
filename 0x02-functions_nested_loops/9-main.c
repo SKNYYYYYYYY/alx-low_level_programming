7213aa3dd6267aee72bc7e20d0f825cbfa49252a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-10-12 06:20:06 -0700

    “script”

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,49 +1,12 @@
 #include "main.h"
+
 /**
- *  * times_table - a function that prints the times table of 9
- *   * @void: the function receives no arguments
- *    *
- *     * Return: no return value
-*/
-void times_table(void)
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
 {
-	int i, a, x;
-
-
-	for (i = 0; i < 10; i++)
-	{
-		for (a = 0; a < 10; a++)
-		{
-			x = a * i;
-			if (a == 0)
-			{
-				if (i == 0)
-				{
-					_putchar(x + '0');
-				}
-				else
-				{
-					_putchar('\n');
-					_putchar(x + '0');
-				}
-			}
-			else if (x >= 10)
-			{
-				_putchar(' ');
-				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(x + '0');
-			}
-			if (a != 9)
-			{
-				_putchar(',');
-			}
-		}
-	}
-	_putchar('\n');
+	    times_table();
+	        return (0);
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ *  *  * times_table - a function that prints the times table of 9
+ *   *   * @void: the function receives no arguments
+ *    *    *
+ *     *     * Return: no return value
+  */
+void times_table(void)
+{
+int i, a, x;
+
+for (i = 0; i < 10; i++)
+{
+for (a = 0; a < 10; a++)
+{
+x = a * i;
+if (a == 0)
+{
+if (i == 0)
+{
+_putchar(x + '0');
+}
+else
+{
+_putchar('\n');
+_putchar(x + '0');
+}
+}
+else if (x >= 10)
+{
+_putchar(' ');
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(x + '0');
+}
+if (a != 9)
+{
+_putchar(',');
+}
+}
+}
+_putchar('\n');
+}
