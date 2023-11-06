5a81ba596305c177baf8d92185ff20d5e6da6370
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-11-06 06:30:00 -0800

    Betty corrected

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include<stdlib.h>
 #include<stdio.h>
 /**
  * main - printing the file name of the file the program is compiled from
+ *@void - it receives no arguments
  * Return:  0 so that the program exites
  */
 int main(void)
 {
-	printf(__FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
