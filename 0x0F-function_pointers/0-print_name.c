cdc841ec919265165d34d488a0f608e5363e3e76
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-11-08 10:01:03 -0800

    corrected betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - function that prints a name
- * @name: pointer to persons name
+ * print_name - function that prints a name hhhhj h hhhh hhh  hhhh 
+ * @name: pointer to persons name uuiii iiiui iiuii 
  * @f: pointer to a function that takes char  pointer as a parameter
  */
 void print_name(char *name, void (*f)(char *))
