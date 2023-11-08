5fb635b9ec2e9aec929a20c945cb95f8dd6aee02
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2023-11-08 10:08:56 -0800

    corrected betty

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-	#include <stdlib.h>
-
-
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+#include <stdlib.h>
+/**
+ * main - prints its own opcodes fff jjjj jjj jj 
+ * f
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+* Return: Always 0 (Success)
+ */
 	int main(int argc, char *argv[])
 	{
 		int bytes, i;
