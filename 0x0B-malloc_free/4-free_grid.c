72ab6e63002aa15e6e3d3a7fa38652c0c6346d80
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-07-16 14:32:18 -0700

    correction

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
+
 /**
- *free_grind - frees the previous mem allocated
-*@grind: pointer to a 2d array
-*@height: height of a 2d array
-*/
+ * free_grid - frees the previous mem allocated
+ * @grid: pointer to a 2d array
+ * @height: height of a 2d array
+ */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    for(i=0;i<height;i++)
-	free(grid[i]);
-    free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
