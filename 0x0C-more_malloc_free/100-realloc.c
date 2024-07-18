ed68e15f3ac8ea8faf5cc87049beb5c172987365
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-07-18 16:01:44 -0700

    Correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,10 +32,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptr_old = ptr;
 	if (new_size < old_size)
 		old_size = new_size;
-
-		r = sizeof(int);
-		for (i = 0; i < (old_size / r) && i < (new_size / sizeof(int)); i++)
-			ptr_new[i] = ptr_old[i];
+	r = sizeof(int);
+	for (i = 0; i < (old_size / r) && i < (new_size / sizeof(int)); i++)
+		ptr_new[i] = ptr_old[i];
 
 
 	free(ptr_old);
