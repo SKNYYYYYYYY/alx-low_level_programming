bd66835a6939a8d666c7cca4e96e5db7202e5c2f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-08-08 23:38:07 +0300

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,15 +6,15 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int i = 0, k = 0;
-    if(b == NULL)
+unsigned int i = 0, k = 0;
+if(b == NULL)
+    return (0);
+for(i = 0; b[i] != '\0'; i++)
+{
+    if(b[i] != '0' && b[i] != '1')
         return (0);
-    for(i = 0; b[i] != '\0'; i++)
-    {
-        if(b[i] != '0' && b[i] != '1')
-            return (0);
-        k = (k << 1) | (b[i] - '0');
-    }
+    k = (k << 1) | (b[i] - '0');
+}
 
     return (k);
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+*print_binary - function to convert decimal to binary 
+*@n: decimal number
+*/
+void print_binary(unsigned long int n)
+{
+    unsigned int k, temp = n;
+    int i, len = 0;
+    while (temp > 0)
+    {
+        len++;
+        temp /= 2;
+    }
+    if(n == 0)
+        _putchar('0');
+    for (i = len - 1; i >= 0; i--)
+    {
+            k = n >> i;
+            if(k & 1)
+                _putchar('1');
+            else
+                _putchar('0');
+    }
+}
