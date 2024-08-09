68e299af376038fb5ec86bb85b5f2f9ffa78fc99
root <root@JMUGAMBI-UNDWRT.firstassurance.co.ke>
2024-08-09 10:09:10 +0300

    task 1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,12 +3,13 @@
 *get_bit - function to get the bit of n at index index
 * @n: the number to get the bit from
 *@index: index of the bit
-*Return:  - return k & 1; meaning either 0 or 1
+* Return:  - return k & 1; meaning either 0 or 1
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int k;
- 	if (n < index || index > (sizeof(unsigned long int) * 8))
+
+	if (n < index || index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	k = n >> index;
