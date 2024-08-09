545c0d97c7557f96b4058f182fec4422f4e1b6d6
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-08-09 16:28:56 +0300

    checking for endiann

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,15 @@
 #include "main.h"
+/**
+ * get_endianness - check for the endianess of your pc
+ * Return: 1 if lil endian and 0 if big
+ */
 int get_endianness(void)
 {
 	unsigned int k = 1;
 
 	char *c = (char *)&k;
-	if(*c)
+
+	if (*c)
 		return (1);
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
