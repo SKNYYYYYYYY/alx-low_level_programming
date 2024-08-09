fdc9a45f818baa1503313054ed9ad0709c1fb3fa
root <root@JMUGAMBI-UNDWRT.firstassurance.co.ke>
2024-08-09 10:05:36 +0300

    task 1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,10 +7,10 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int k;
-    if (n < index || index > (sizeof(unsigned long int) * 8))
-        return (-1);
+	unsigned long int k;
+ 	if (n < index || index > (sizeof(unsigned long int) * 8))
+		return (-1);
 
-    k = n >> index;
-    return (k & 1);
+	k = n >> index;
+	return (k & 1);
 }
