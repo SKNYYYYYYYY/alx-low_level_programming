a6605bdbdd629fe1e8eeca6419c38c60ed426557
root <root@JMUGAMBI-UNDWRT.firstassurance.co.ke>
2024-08-09 12:10:07 +0300

    task 1

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,13 +2,14 @@
 /**
 *clear_bit - function to flip a 1 bit at a certain index
 *@n: pointer to the memory location
-*Return: return 1 in success and -1 in fail
+*@index: index starting from 0
+* Return: return 1 in success and -1 in fail
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if(index >= sizeof(unsigned long int) * 8)
-        return (-1);
-    if (*n & (1 << index))
-        *n = *n ^ (1 << index);
-    return (1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	if (*n & (1 << index))
+		*n = *n ^ (1 << index);
+	return (1);
 }
