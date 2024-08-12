062e40cf016d0959cf07c5e95323a6183a84dab9
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-08-13 00:14:12 +0300

    task-2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
-*read_textfile - file to read and write contents of a file
-*@filename: file name
-*@letters: number of letters to write
-*Return: return number of printed chars
-*/
+ * read_textfile - Reads and writes the contents of a file to stdout.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to write.
+ *
+ * Return: The number of printed characters.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
-	ssize_t  number_of_chars = 0;
+	ssize_t number_of_chars = 0;
 	int character;
 
+	if (filename == NULL)
+		return (0);
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
-	while ((character = fgetc(fp)) != EOF)
+	while ((character = fgetc(fp)) != EOF && number_of_chars < (ssize_t)letters)
 	{
-	if (number_of_chars <= (ssize_t)letters)
-	{
-		
 		fputc(character, stdout);
 		number_of_chars++;
-
-	}
 	}
 	fclose(fp);
-return (number_of_chars);
+	return (number_of_chars);
 }
