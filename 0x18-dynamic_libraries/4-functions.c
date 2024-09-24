0d18c041fa52b8bb1dc05badddd3cefdae079943
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-24 11:00:29 +0300

    dynamic libraries

--- 0x18-dynamic_libraries/4-functions.c
+++ 0x18-dynamic_libraries/4-functions.c
@@ -1,20 +1,60 @@
 #include "main.h"
+/**
+ * _memcpy - copies a block of memory from source to destination
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * @n: the number of bytes to copy
+ *
+ * Return: pointer to the destination buffer
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
 }
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to locate
+ *
+ * Return: pointer to the first occurrence of the
+ *  character, or NULL if not found
+ */
 char *_strchr(char *s, char c)
 {
 
 }
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search
+ * @accept: the characters to include in the prefix
+ *
+ * Return: number of bytes in the initial segment
+ * of s consisting only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 
 }
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the bytes to look for
+ *
+ * Return: pointer to the byte in s that matches
+ * one of the bytes in accept, or NULL if no match
+ */
 char *_strpbrk(char *s, char *accept)
 {
 
 }
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: the string to search
+ * @needle: the substring to locate
+ *
+ * Return: pointer to the beginning of the located
+ * substring, or NULL if not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
 
