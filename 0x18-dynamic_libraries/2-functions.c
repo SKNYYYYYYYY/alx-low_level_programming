3bf8291e11747024bf12a88ece953d6ae3ca9f29
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2024-09-24 10:57:41 +0300

    dynamic libraries

--- 0x18-dynamic_libraries/2-functions.c
+++ 0x18-dynamic_libraries/2-functions.c
@@ -1,20 +1,56 @@
 #include "main.h"
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: the character to be checked
+ *
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
 
 }
+/**
+ * _strlen - calculates the length of a string
+ * @s: the string to be measured
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-
+int x = 0;
+while (s[x] != '\0')
+{
+x++;
 }
+return (x);
+}
+/**
+ * _puts - prints a string to standard output
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
 void _puts(char *s)
 {
 
 }
+/**
+ * _strcpy - copies a string from source to destination
+ * @dest: the destination buffer
+ * @src: the source string
+ *
+ * Return: pointer to the destination string
+ */
 char *_strcpy(char *dest, char *src)
 {
 
 }
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the integer value of the string
+ */
 int _atoi(char *s)
 {
 
--- 0x18-dynamic_libraries/3-functions.c
+++ 0x18-dynamic_libraries/3-functions.c
@@ -1,20 +1,60 @@
 #include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string to be appended
+ *
+ * Return: pointer to the resulting string
+ */
 char *_strcat(char *dest, char *src)
 {
 
 }
+/**
+ * _strncat - concatenates two strings up to a specified number of characters
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of characters to append
+ *
+ * Return: pointer to the resulting string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 
 }
+/**
+ * _strncpy - copies up to n characters from the source
+ *  string to the destination string
+ * @dest: the destination buffer
+ * @src: the source string
+ * @n: the number of characters to copy
+ *
+ * Return: pointer to the destination string
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 
 }
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 0 if the strings are identical, a negative
+ *  or positive value if they differ
+ */
 int _strcmp(char *s1, char *s2)
 {
 
 }
+/**
+ * _memset - fills a block of memory with a constant byte
+ * @s: the memory area to be filled
+ * @b: the byte to fill with
+ * @n: the number of bytes to fill
+ *
+ * Return: pointer to the memory area
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 
