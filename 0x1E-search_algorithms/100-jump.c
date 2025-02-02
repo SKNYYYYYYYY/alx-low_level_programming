5fa8ce08bb5ca752ad58d5d53f57ce40d3569d1f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-02-02 18:12:23 +0300

    jump search algorithm master

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+/**
+ * jump_search - divide and conquer algorithm to search for a value in an array
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: index of the value
+ */
+int jump_search(int *array, size_t size, int value){
+	size_t current = 0, jumper = sqrt(size), i;
+
+	if(!array)
+		return (-1);
+	while (current < size)
+	{
+		if (value == array[current])
+			return (current);
+
+		printf("Value checked array[%d] = [%d]\n", current, array[current]);
+		if (value <= (array[current + jumper]))
+		{
+			printf("Value found between indexes [%d] and [%d]\n", current, current + jumper);
+
+			for (i = current; i <= (current + jumper); i++)
+			{
+				if (i < size)
+					printf("Value checked array[%d] = [%d]\n", i, array[i]);
+				else
+					return (-1);
+				if (value == array[i])
+					return (i);
+			}
+			return (current);
+		}
+	current += jumper;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,9 @@
 /*Headers*/
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <math.h>
 /*Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
-
+int jump_search(int *array, size_t size, int value);
 #endif
