bbb39760e3187d5a5e6266ea8f6fab658e420a16
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-02-02 21:14:35 +0300

    interpolation search algorithm master

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,21 +8,15 @@ int interpolation_search(int *array, size_t size, int value)
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		position = (low + ((double)(value - array[low]) / (array[high] - array[low])) * (high - low));
-		printf("Value checked array[%ld]", position);
-		if 	(position >= size)
-		{
-			printf(" is out of range\n");
-			return (-1);
-		}
+		printf("Value checked array[%ld]= [%d]\n", position, array[position]);
 		if (value == array[position])
-		{
-			printf(" = [%d]\n", array[position]);
 			return (position);
-		}
 		if (value < array[position])
 			high = position - 1;
 		else
 			low = position + 1;
 	}
+	position = (low + ((double)(value - array[low]) / (array[high] - array[low])) * (high - low));
+	printf("Value checked array[%ld] is out of range\n", position);
 	return (-1);
 }
